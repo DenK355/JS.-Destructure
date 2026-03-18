6b7953c4626ddfee733806a38ed04fed568b393a
Denis <bezsaluy@gmail.com>
2026-03-18 13:59:49 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
 'use strict'
+
+
+
+const books = {
+
+    fantasy: [
+
+        {
+            id: 1,
+            title: 'Harry Potter',
+            author: 'Joanne Rowling',
+            year: 1991,
+        },
+
+        {
+            id: 2,
+            title: 'The Lord of the Rings',
+            author: 'J.R.R. Tolkien',
+            year: 1954,
+        },
+
+        {
+            id: 3,
+            title: 'Game of Thrones',
+            author: 'George R.R. Martin',
+            year: 1996,
+        },
+
+        {
+            id: 4,
+            title: 'The Witcher',
+            author: 'Andrzej Sapkowski',
+            year: 1986,
+        },
+
+        {
+            id: 5,
+            title: 'Stormlight',
+            author: 'Brandon Sanderson',
+            year: 2010,
+        },
+
+    ],
+
+    scienceFiction: [
+
+        {
+            id: 1,
+            title: 'Dune',
+            author: 'Frank Herbert',
+            year: 1965,
+        },
+
+        {
+            id: 2,
+            title: 'Foundation',
+            author: 'Isaac Asimov',
+            year: 1951,
+        },
+
+        {
+            id: 3,
+            title: '2001: A Space Odyssey',
+            author: 'Arthur C. Clarke',
+            year: 1968,
+        },
+
+        {
+            id: 4,
+            title: 'Stranger in a Strange Land',
+            author: 'Robert Heinlein',
+            year: 1961,
+        },
+
+        {
+            id: 5,
+            title: 'Solaris',
+            author: 'Stanislav Lem',
+            year: 1961,
+        },
+
+    ],
+
+}
+
+const [thirdBook, writer] = [books.fantasy[2], books.scienceFiction[3].author];
+
+console.log(thirdBook);
+console.log(writer);
